8a19b50be1d04655507989673c10b5bcbb4fb491
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-28 20:21:05 -0500

    Update Create.js

--- forms/Create/Create.js
+++ forms/Create/Create.js
@@ -2,16 +2,19 @@ req = ""
 query = ""
 results = ""
 let pw = "14421442Kh"
-let userName = 'kmh76825'
+let userName = `'kmh76825`
 
 btnCreate.onclick = function() {
   let newUsername =  inptUsernameCreate.value
   let newPassword = inptPasswordCreate.value
   let newEmail = inptEmail.value
-  query = `INSERT INTO user VALUES ('${newUsername}, ${newEmail}, ${newPassword})`
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=kmh76825&query=" + query)
-
+  query = `INSERT INTO user (username, email, password) 
+VALUES('${newUsername}','${newEmail}','${newPassword}')`
+  console.log(query)
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query)
+console.log(req.status)
   if (req.status == 200) { //transit worked.
+  console.log(req.responseText)
     if (req.responseText == 500) { // means the insert succeeded
       console.log("You have successfully added the user!")
     } else
