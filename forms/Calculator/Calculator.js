6472b9704d670522e2a757c974561b114a03f882
Daniela Bresler <dbresler@github.com>
2020-11-01 16:45:33 -0600

    Daniela goal updates

--- forms/Calculator/Calculator.js
+++ forms/Calculator/Calculator.js
@@ -1,3 +1,32 @@
 btnCalculatorBack.onclick=function(){
   ChangeForm(Home)
 }
+
+let requestURL = "simplecalculatorbygkservice.herokuapp.com/api/agritaskcalculator/result/{expression}?customerId=111980&productId=1504795"
+function onXHRLoad() {
+    let message = ""
+    let apiData = JSON.parse(this.responseText)
+    for (i = 0; i <= apiData.results.length - 1; i++) {
+        console.log(`${apiData.results[i].name}`)
+        message = message + apiData.results[i].name + "\n"
+    }
+    txtCal.value = message
+    // if want to add to database call a function here that does that
+    // addToDatabase()
+}
+
+
+/*
+function callAPI(URL) {
+    var xhttp = new XMLHttpRequest();
+    
+// make the API request
+    xhttp.addEventListener('load', onXHRLoad)
+    xhttp.send()
+}
+
+btnCal.onclick=function(){
+   // call the API calling code above
+  callAPI(requestURL)
+}
+*/
--- forms/Goals/Goals.js
+++ forms/Goals/Goals.js
@@ -12,7 +12,7 @@ Goals.onshow=function(){
           results1 = JSON.parse(req.responseText)
 }
 
-btnnewGoals.onclick=function(){
-  inptAmount = 
+
+drpBudget.onclick=function(){
   
 }
