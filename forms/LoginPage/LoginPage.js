9753da4f43ca664825c326f4ba22f6ee1ddc1cf9
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-11-02 16:46:13 -0600

    Update LoginPage.js

--- forms/LoginPage/LoginPage.js
+++ forms/LoginPage/LoginPage.js
@@ -8,64 +8,54 @@ var valid =  false;
 btnLogin.onclick=function() {
   let inputUsername = inptUsername.value
   let inputPassword = inptPassword.value
- query1 = `SELECT  username FROM user`
+ query1 = `SELECT username FROM user`
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query1)
   if (req.status == 200) { //transit worked.
           console.log(req.status)
           console.log(req.responseText)
-          results1 = JSON.parse(req.responseText)
-          console.log(results1)
-           query2 = `SELECT  password FROM user`
+          usernames = JSON.parse(req.responseText)
+          console.log(usernames)
+           query2 = `SELECT password FROM user`
         req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query2)
           if (req.status == 200) { //transit worked.
             console.log(req.status)
             console.log(req.responseText)
-            results2 = JSON.parse(req.responseText)
-            console.log(results2)
-                function validate(inputUsername,inputPassword) {
-                    for (var i=0; i <results1.length; i++) {
-                        if ((inputUsername == results1[i]) && (inputPassword == results2[i])) {
-                           valid = true;
-                            break; 
+            passwords = JSON.parse(req.responseText)
+            console.log(passwords)
+			validate(inputUsername, inputPassword);
+                function validate(username,password) {
+                    for (var i=0; i <usernames.length; i++) {
+                        if ((inputUsername == usernames[i]) && (inputPassword == passwords[i])) {
+                            valid = true;
+                            break;  
 						}
 						}
-                                                }
-					
-				
-						
-						
-    						if (valid) {
-												alert("Valid Credentials")
+						}	
+    					if (valid) {
+							alert("Valid Credentials")
+							ChangeForm(Home)
                                                     query3 = `SELECT  user_id FROM user WHERE  inputUsername == results1[i]`
-                                                  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", 				"host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query3)
-                                                  /*if (req.status == 200) { //transit worked.
+                                                  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query3)
+                                                  if (req.status == 200) { //transit worked.
                                                     console.log(req.status)
-                                                    console.log(req.responseText) */
+                                                    console.log(req.responseText) 
                                                     userID= JSON.parse(req.responseText)
-                                                    console.log(userID)
-                                                    ChangeForm(Home);
-                                                  /* } else { 
+                                                      console.log(userID) 
+                                                       } else { 
                                                     alert(`transit error`)
-        
-                                             } */
-                                             
-    						} else {
+                                                      }
+    					} else {
         					alert("Invalid Credentials")
-    						} 
-		  } else {
-				// transit error
-    	console.log(`Error: ${req.status}`);
-		  }
-							
-		  
-  } else {
-   		// transit error
-    	console.log(`Error: ${req.status}`);
-  }
-
-
-  }
-
+    					}       
+			} else {
+							  // transit error
+    		console.log(`Error: ${req.status}`);
+							}			
+   	} else { 
+			// transit error
+    		console.log(`Error: ${req.status}`);
+			}
+  } 
 
 lblCreateAccount.onclick=function(){
   ChangeForm(Create)
