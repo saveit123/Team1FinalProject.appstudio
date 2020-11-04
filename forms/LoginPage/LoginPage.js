e95303814fd1dc8594bcb07232c431b8e9843277
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-11-04 14:41:08 -0600

    Update LoginPage.js

--- forms/LoginPage/LoginPage.js
+++ forms/LoginPage/LoginPage.js
@@ -1,21 +1,15 @@
-let pw = "14421442Kh"
-let userName = `'kmh76825`
-req = ""
-query = ""
-results = ""
-var valid =  false;
 
 btnLogin.onclick=function() {
   let inputUsername = inptUsername.value
   let inputPassword = inptPassword.value
- query1 = `SELECT username FROM user`
+ query = `SELECT username FROM user`
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query1)
   if (req.status == 200) { //transit worked.
           console.log(req.status)
           console.log(req.responseText)
           usernames = JSON.parse(req.responseText)
           console.log(usernames)
-           query2 = `SELECT password FROM user`
+           query = `SELECT password FROM user`
         req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query2)
           if (req.status == 200) { //transit worked.
             console.log(req.status)
