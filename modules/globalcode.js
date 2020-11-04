2947a2a7df855ef8404f2ec4a80f4cd9bbb9dc02
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-11-04 14:41:11 -0600

    Create globalcode.js

---
+++ modules/globalcode.js
@@ -0,0 +1,11 @@
+let userID = ""
+let pw = "14421442Kh"
+let userName = `'kmh76825`
+let req = ""
+let query = ""
+let results = ""
+var valid =  false;
+
+let goalsMonth = ['']
+let goalsWeek= ['']
+let currentGoal = ""
